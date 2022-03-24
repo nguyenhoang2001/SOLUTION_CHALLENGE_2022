79c62cfc575c361c7b957b572068fae5dc8cdbc4
nguyentheloc-310101 <nguyentheloc310101@gmail.com>
2022-03-24 11:21:17 +0700

    Update UserInfo.dart

--- lib/views/userInfo/UserInfo.dart
+++ lib/views/userInfo/UserInfo.dart
@@ -15,38 +15,120 @@ class UserInfo extends StatefulWidget {
 }
 
 class _UserInfoState extends State<UserInfo> {
+  final double coverHeight = 280;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          'Your Infomation',
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
+      body: SafeArea(
+          child: Column(
+            children: [
+              //for circle avtar image
+              _getHeader(),
+              SizedBox(
+                height: 10,
+              ),
+              _profileName("Name of user"),
+              SizedBox(
+                height: 14,
+              ),
+              _heading("Personal Details"),
+              SizedBox(
+                height: 6,
+              ),
+              _detailsCard(),
+              SizedBox(
+                height: 10,
+              ),
+
+              SizedBox(
+                height: 6,
+              ),
+
+            ],
+          )),
+    );
+  }
+
+  Widget _getHeader() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              //borderRadius: BorderRadius.all(Radius.circular(10.0)),
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: NetworkImage(
+                        "https://cdn.pixabay.com/photo/2016/08/08/09/17/avatar-1577909__340.png"))
+              // color: Colors.orange[100],
+            ),
           ),
         ),
+      ],
+    );
+  }
 
+  Widget _profileName(String name) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.80, //80% of width,
+      child: Center(
+        child: Text(
+          name,
+          style: TextStyle(
+              color: Colors.black, fontSize: 30, fontWeight: FontWeight.w800),
+        ),
       ),
-      body: Center(
+    );
+  }
+
+  Widget _heading(String heading) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.80, //80% of width,
+      child: Text(
+        heading,
+        style: TextStyle(fontSize: 25),
+      ),
+    );
+  }
+
+  Widget _detailsCard() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Card(
+        elevation: 4,
         child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            Image.asset(
-              'assets/images/empty.png',
-              height: 300.0,
-              width: 300.0,
+          children: [
+            //row for each deatails
+            ListTile(
+              leading: Icon(Icons.email),
+              title: Text("Something@gmail.com"),
             ),
-            Text(
-              'Nothing is here',
-              style: TextStyle(
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold,
-              ),
+            Divider(
+              height: 0.6,
+              color: Colors.black87,
             ),
+            ListTile(
+              leading: Icon(Icons.phone),
+              title: Text("1234567890"),
+            ),
+            Divider(
+              height: 0.6,
+              color: Colors.black87,
+            ),
+            ListTile(
+              leading: Icon(Icons.cake),
+              title: Text("date of birth"),
+            )
           ],
         ),
       ),
     );
   }
+
+
 }
